1c70410b01823761e3b5367366458039602f3086
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2022-05-19 23:32:40 +0700

    :bug: fix: use axios instead of line sdk

--- api/dist/app.js
+++ api/dist/app.js
@@ -4,20 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-//import axios from 'axios'
-const bot_sdk_1 = __importDefault(require("@line/bot-sdk"));
 const fs_1 = __importDefault(require("fs"));
 const https_1 = __importDefault(require("https"));
+const axios_1 = __importDefault(require("axios"));
 //set port
 const port = process.env.port || 3000;
 //https
 const privkey = fs_1.default.readFileSync("/etc/letsencrypt/live/api.guntxjakka.me/privkey.pem");
 const cert = fs_1.default.readFileSync("/etc/letsencrypt/live/api.guntxjakka.me/fullchain.pem");
 const credentials = { key: privkey, cert: cert };
-//init line client from line sdk
-const client = new bot_sdk_1.default.Client({
-    channelAccessToken: process.env.CAT
-});
+/* //init line client from line sdk
+const client = new line.Client({
+    channelAccessToken: process.env.CAT as string
+}) */
+const channelAccessToken = process.env.CAT;
 //init express app
 const app = (0, express_1.default)();
 //use json middleware
@@ -26,16 +26,32 @@ app.use(express_1.default.json());
 app.get('/test', (req, res) => {
     const userId = req.query.id;
     const message = {
-        type: 'text',
-        text: 'Hello World.'
+        to: userId,
+        messages: [
+            {
+                type: 'text',
+                text: 'Hello World.'
+            }
+        ]
     };
-    client.pushMessage(userId, message)
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${channelAccessToken}`
+    };
+    axios_1.default.post('https://api.line.me/v2/bot/message/push', message, headers)
         .then(() => {
         res.send(`Sent message to ${userId}`);
     })
         .catch(err => {
         console.log(err);
     });
+    /* client.pushMessage(userId, message as any)
+        .then(() => {
+            res.send(`Sent message to ${userId}`)
+        })
+        .catch(err => {
+            console.log(err)
+        }) */
 });
 /* app.listen(port, () => {
     console.log(`App listening on port ${port}`)
--- api/src/app.ts
+++ api/src/app.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from 'express'
 import line from '@line/bot-sdk'
 import fs from 'fs'
 import https from 'https'
+import axios from 'axios'
 
 //set port
 const port = process.env.port || 3000
@@ -12,10 +13,12 @@ const privkey = fs.readFileSync("/etc/letsencrypt/live/api.guntxjakka.me/privkey
 const cert = fs.readFileSync("/etc/letsencrypt/live/api.guntxjakka.me/fullchain.pem")
 const credentials = { key: privkey, cert: cert }
 
-//init line client from line sdk
+/* //init line client from line sdk
 const client = new line.Client({
     channelAccessToken: process.env.CAT as string
-})
+}) */
+
+const channelAccessToken = process.env.CAT
 
 //init express app
 const app = express()
@@ -27,16 +30,33 @@ app.use(express.json())
 app.get('/test', (req: Request, res: Response) => {
     const userId = req.query.id as string
     const message = {
-        type: 'text',
-        text: 'Hello World.'
+        to: userId,
+        messages: [
+            {
+                type: 'text',
+                text: 'Hello World.'
+            }
+        ]
     }
-    client.pushMessage(userId, message as any)
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${channelAccessToken}`
+
+    }
+    axios.post('https://api.line.me/v2/bot/message/push', message, headers as any)
         .then(() => {
             res.send(`Sent message to ${userId}`)
         })
         .catch(err => {
             console.log(err)
         })
+    /* client.pushMessage(userId, message as any)
+        .then(() => {
+            res.send(`Sent message to ${userId}`)
+        })
+        .catch(err => {
+            console.log(err)
+        }) */
 })
 
 /* app.listen(port, () => {
