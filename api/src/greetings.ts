d8e4b41cb980fd5af15be95894b99bc9af0449e1
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2022-05-26 23:03:45 +0700

    :recycle: refactor(api): change greeting text again

--- api/src/greetings.ts
+++ api/src/greetings.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { TextMessage } from '@line/bot-sdk'
 
 export async function sendGreetingMessage(userId: string, channelAccessToken: string, displayName: string) {
-    const greetingMessage: TextMessage = { type: 'text', text: `สวัสดีคุณ ${displayName}! ขอบคุณที่เพิ่มเราเป็นเพื่อน☺️🎉\n\n โปรดเพิ่มข้อมูลของคุณในระบบได้ที่ลิงค์ด้านล่าง 👇\n https://parcetrace.vercel.app/regis?userId=${userId}` }
+    const greetingMessage: TextMessage = { type: 'text', text: `สวัสดีคุณ ${displayName}!\n ขอบคุณที่เพิ่มเราเป็นเพื่อน☺️🎉\n\n โปรดเพิ่มข้อมูลของคุณในระบบได้ที่ลิงค์ด้านล่าง 👇\n https://parcetrace.vercel.app/regis?userId=${userId}` }
     const headers = { 'Authorization': `Bearer ${channelAccessToken}`, 'Content-Type': 'application/json' }
     const body = {
         to: userId,
