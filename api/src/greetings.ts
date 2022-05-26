459628138eb57134b8fd65e2f12e83696bdd5142
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2022-05-26 23:00:38 +0700

    :bug: fix(api): LINE emoji not displaying properly

--- api/src/greetings.ts
+++ api/src/greetings.ts
@@ -2,23 +2,11 @@ import axios from 'axios'
 import { TextMessage } from '@line/bot-sdk'
 
 export async function sendGreetingMessage(userId: string, channelAccessToken: string, displayName: string) {
-    const greetingMessage: TextMessage = { type: 'text', text: `สวัสดีคุณ ${displayName}! ขอบคุณที่เป็นเพื่อนกับเรา$\n โปรดเพิ่มข้อมูลในระบบได้ที่ลิงค์ด้านล่าง$\n https://parcetrace.vercel.app/regis?userId=${userId}` }
+    const greetingMessage: TextMessage = { type: 'text', text: `สวัสดีคุณ ${displayName}! ขอบคุณที่เป็นเพื่อนกับเรา☺️🎉\n โปรดเพิ่มข้อมูลในระบบได้ที่ลิงค์ด้านล่าง 👇\n https://parcetrace.vercel.app/regis?userId=${userId}` }
     const headers = { 'Authorization': `Bearer ${channelAccessToken}`, 'Content-Type': 'application/json' }
     const body = {
         to: userId,
         messages: [greetingMessage],
-        emojis: [
-            {
-                index: 0,
-                productId: '5ac1bfd5040ab15980c9b435',
-                emojiId: '001',
-            },
-            {
-                index: 1,
-                productId: '5ac21e6c040ab15980c9b444',
-                emojiId: '020'
-            }
-        ]
     }
     return axios.post('https://api.line.me/v2/bot/message/push', body, { headers: headers })
 }
