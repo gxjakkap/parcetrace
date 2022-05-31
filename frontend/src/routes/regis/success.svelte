42f339b042c0d508124bf4cdfa71b6486f03542e
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2022-05-31 16:14:32 +0700

    :sparkles: feat(fe): add regis success page

--- frontend/src/routes/regis/success.svelte
+++ frontend/src/routes/regis/success.svelte
@@ -3,5 +3,18 @@
 </svelte:head>
 
 <main>
-    <p>Success</p>
+        <div class="bg-gray-200 min-h-screen flex flex-col">
+            <div
+                class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2"
+            >
+                <div
+                    class="bg-white px-6 py-8 rounded shadow-md text-black w-full"
+                >
+                    <h1 class="font-Prompt mb-8 text-3xl text-center">
+                        การลงทะเบียนสำเร็จ คุณสามารถปิดหน้านี้ได้
+                    </h1>
+                </div>
+            </div>
+        </div>
 </main>
+
