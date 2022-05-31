a5ffd4d886cf1c012f202d464fd0ef4e8dea78ff
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2022-05-31 16:59:17 +0700

    :recycle: refactor(api): modify register confirm message

--- api/src/message.ts
+++ api/src/message.ts
@@ -3,7 +3,7 @@ import { TextMessage } from '@line/bot-sdk'
 import { userData } from './firestoreoperation'
 
 export async function sendRegistrationConfirmMessage(userId: string, channelAccessToken: string, userData: userData) {
-    const message: TextMessage = { type: 'text', text: `ลงทะเบียนสำเร็จ! ข้อมูลของคุณถูกบันทึกในระบบเรียบร้อยแล้ว🗃️ โปรดตรวจสอบความถูกต้องของข้อมูล หากข้อมูลผิดกรุณาแจ้งผู้ดูแลระบบ\n\nชื่อจริง: ${userData.name}\n\nนามสกุล: ${userData.surname}\n\nเบอร์โทรศัพท์: ${userData.phoneNumber}\n\nเลขห้อง: ${userData.room}` }
+    const message: TextMessage = { type: 'text', text: `ลงทะเบียนสำเร็จ! ข้อมูลของคุณถูกบันทึกในระบบเรียบร้อยแล้ว🗃️\n โปรดตรวจสอบความถูกต้องของข้อมูล หากข้อมูลผิดกรุณาแจ้งผู้ดูแลระบบ\n\nชื่อจริง: ${userData.name}\n\nนามสกุล: ${userData.surname}\n\nเบอร์โทรศัพท์: ${userData.phoneNumber}\n\nเลขห้อง: ${userData.room}` }
     const headers = { 'Authorization': `Bearer ${channelAccessToken}`, 'Content-Type': 'application/json' }
     const body = {
         to: userId,
