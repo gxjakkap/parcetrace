372214f94c88e29d2659aebad5e16239ae490ce0
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2022-06-08 13:55:59 +0700

    :bug: fix(api): wrong type in findUserWithPhoneNumber

--- api/src/firestoreoperation.ts
+++ api/src/firestoreoperation.ts
@@ -61,7 +61,7 @@ export const checkIfDocumentExist = async (ref: FirebaseFirestore.DocumentRefere
 }
 
 export const findUserWithPhoneNumber = async (collection: FirebaseFirestore.CollectionReference, phoneNumber: number) => {
-    const snapshot = await collection.where('phoneNumber', '==', phoneNumber).get()
+    const snapshot = await collection.where('phoneNumber', '==', phoneNumber.toString()).get()
     let response: findUserWithPhoneNumberResponse = { successful: false, statusCode: 500 }
     if (!snapshot.empty) {
         let dataArray: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[] = []
