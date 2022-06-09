1f76da8c31fd2a4db78b15436c85ff8961b36f7b
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2022-06-09 11:25:33 +0700

    :recycle: refactor(fe): encapsulate loading page into component

---
+++ frontend/src/lib/loading.svelte
@@ -0,0 +1,23 @@
+<section>
+    <div class="bg-gray-200 dark:bg-slate-800 min-h-screen flex flex-col">
+        <div
+            class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2"
+        >
+            <div
+                class="bg-white dark:bg-slate-700 px-6 py-8 rounded shadow-md text-black w-full"
+            >
+                <div class="flex justify-center items-center">
+                    <div
+                        class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
+                        role="status"
+                    />
+                </div>
+                <h1
+                    class="font-Prompt text-black dark:text-white mb-8 text-3xl text-center"
+                >
+                    กำลังโหลดข้อมูล
+                </h1>
+            </div>
+        </div>
+    </div>
+</section>
