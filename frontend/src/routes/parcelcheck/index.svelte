8ec5dc4a727778079a0a78c2f5471e27258f9032
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2022-06-09 11:18:56 +0700

    :sparkles: feat(fe): add loading to parcelcheck/index for continuity

--- frontend/src/routes/parcelcheck/index.svelte
+++ frontend/src/routes/parcelcheck/index.svelte
@@ -9,6 +9,8 @@
 
     let phoneNumber: number | null;
 
+    let loadingState = false;
+
     async function onSubmit() {
         fetch(`https://${apiUrl}/getUserId?phoneNo=${phoneNumber}`, {
             method: "GET",
@@ -37,36 +39,60 @@
 </svelte:head>
 
 <main>
-    <div class="bg-gray-200 dark:bg-slate-800 min-h-screen flex flex-col">
-        <div
-            class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2"
-        >
+    {#if !loadingState}
+        <div class="bg-gray-200 dark:bg-slate-800 min-h-screen flex flex-col">
             <div
-                class="bg-white dark:bg-gray-700 px-6 py-8 rounded shadow-md text-black w-full"
+                class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2"
             >
-                <h1
-                    class="font-Prompt mb-8 text-3xl text-center text-black dark:text-white"
+                <div
+                    class="bg-white dark:bg-gray-700 px-6 py-8 rounded shadow-md text-black w-full"
                 >
-                    ตรวจสอบพัสดุ
-                </h1>
-                <input
-                    type="tel"
-                    class="font-Prompt block border border-gray-400 dark:border-gray-700 text-black dark:text-white w-full p-3 rounded mb-4 bg-white dark:bg-slate-500"
-                    placeholder="เบอร์โทรศัพท์"
-                    on:keypress={(e) => {
-                        if (e.key === "Enter") {
-                            onSubmit();
-                        }
-                    }}
-                    bind:value={phoneNumber}
-                />
+                    <h1
+                        class="font-Prompt mb-8 text-3xl text-center text-black dark:text-white"
+                    >
+                        ตรวจสอบพัสดุ
+                    </h1>
+                    <input
+                        type="tel"
+                        class="font-Prompt block border border-gray-400 dark:border-gray-700 text-black dark:text-white w-full p-3 rounded mb-4 bg-white dark:bg-slate-500"
+                        placeholder="เบอร์โทรศัพท์"
+                        on:keypress={(e) => {
+                            if (e.key === "Enter") {
+                                onSubmit();
+                            }
+                        }}
+                        bind:value={phoneNumber}
+                    />
 
-                <button
-                    type="submit"
-                    class="font-Prompt w-full text-center py-3 rounded bg-green-500 text-white hover:bg-green-700 focus:outline-none my-1"
-                    on:click={onSubmit}>ค้นหา</button
-                >
+                    <button
+                        type="submit"
+                        class="font-Prompt w-full text-center py-3 rounded bg-green-500 text-white hover:bg-green-700 focus:outline-none my-1"
+                        on:click={onSubmit}>ค้นหา</button
+                    >
+                </div>
             </div>
         </div>
-    </div>
+    {:else}
+        <div class="bg-gray-200 dark:bg-slate-800 min-h-screen flex flex-col">
+            <div
+                class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2"
+            >
+                <div
+                    class="bg-white dark:bg-slate-700 px-6 py-8 rounded shadow-md text-black w-full"
+                >
+                    <div class="flex justify-center items-center">
+                        <div
+                            class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
+                            role="status"
+                        />
+                    </div>
+                    <h1
+                        class="font-Prompt text-black dark:text-white mb-8 text-3xl text-center"
+                    >
+                        กำลังโหลดข้อมูล
+                    </h1>
+                </div>
+            </div>
+        </div>
+    {/if}
 </main>
