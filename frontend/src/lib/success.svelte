1a75903f52e91ac96ea657554baabafacd4f78a3
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2022-06-09 11:33:47 +0700

    :recycle: refactor(fe): encapsulate success page as a omponent

---
+++ frontend/src/lib/success.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let message: string;
+</script>
+
+<div class="bg-gray-200 min-h-screen flex flex-col">
+    <div
+        class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2"
+    >
+        <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+            <h1 class="font-Prompt mb-8 text-3xl text-center">
+                {message}
+            </h1>
+        </div>
+    </div>
+</div>
--- frontend/src/routes/regis/success.svelte
+++ frontend/src/routes/regis/success.svelte
@@ -1,20 +1,11 @@
+<script lang="ts">
+    import Success from "$lib/success.svelte";
+</script>
+
 <svelte:head>
     <title>Register Succeed - Parcetrace</title>
 </svelte:head>
 
 <main>
-        <div class="bg-gray-200 min-h-screen flex flex-col">
-            <div
-                class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2"
-            >
-                <div
-                    class="bg-white px-6 py-8 rounded shadow-md text-black w-full"
-                >
-                    <h1 class="font-Prompt mb-8 text-3xl text-center">
-                        การลงทะเบียนสำเร็จ คุณสามารถปิดหน้านี้ได้
-                    </h1>
-                </div>
-            </div>
-        </div>
+    <Success message="การลงทะเบียนสำเร็จ คุณสามารถปิดหน้านี้ได้" />
 </main>
-
