85bfa6b64a438936decc0865773b98caf78d9a40
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2022-06-17 22:49:57 +0700

    :coffin: chore(api): purge unused imports

--- api/dist/app.js
+++ api/dist/app.js
@@ -27,7 +27,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-//import line from '@line/bot-sdk'
 const app_1 = require("firebase-admin/app");
 const firestore_1 = require("firebase-admin/firestore");
 const fs_1 = __importDefault(require("fs"));
