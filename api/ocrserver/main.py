c6e125eea98b41513c8c23883d9f70434783ea28
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2023-03-10 22:29:20 +0700

    :bug: fix(api): limit ram usage for pytorch

--- api/ocrserver/main.py
+++ api/ocrserver/main.py
@@ -2,12 +2,15 @@
 
 from flask import Flask, redirect, url_for, request, jsonify
 import easyocr
+import torch
+
+torch.cuda.set_per_process_memory_fraction(0.4, 0)
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config['JSON_AS_ASCII'] = False
 
-reader = easyocr.Reader(['th', 'en'])
+reader = easyocr.Reader(['th', 'en'], gpu = False)
 
 def eocr_getTextResults(raw):
     ans = []
@@ -17,7 +20,7 @@ def eocr_getTextResults(raw):
 
 @app.route('/easyocr', methods=['POST'])
 def eocr():
-    print(request.values)
+    print(request)
     data = request.json
     result = reader.readtext(data['image'])
     return jsonify(status=200, data=eocr_getTextResults(result))
